202c4f1146693ed9174a3de91a7ff7d90643fe7e
Arulpandi13 <arulpandi9629@gmail.com>
2025-06-18 13:08:54 +0530

    Updated login page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+app.options('*', cors()); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
